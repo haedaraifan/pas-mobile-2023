996c92b84aec35205c3f4ca07347e32aa0a7f6b7
haedaraifan <haedaraifan@gmail.com>
2023-11-03 22:02:18 +0700

    feat [MA] hit coffee api

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pas_mobile_2023/controllers/coffee_list_controller.dart';
 import 'package:pas_mobile_2023/controllers/example_controller.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
+
   final ExampleController _controller = Get.put(ExampleController());
+  final CoffeeListController coffeeListController = Get.put(CoffeeListController());
+
+  Widget coffeeList() {
+    return Obx(() => coffeeListController.isLoading.value ?
+      const Center(
+        child: CircularProgressIndicator()
+      ) :
+      GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        shrinkWrap: true,
+        itemCount: coffeeListController.coffeResponseModel.length,
+        itemBuilder: (context, index) {
+          final product = coffeeListController.coffeResponseModel[index];
+          return Container(
+            color: Colors.green[300],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Image.network(product.imageUrl),
+                  Text(product.name),
+                  Text("\$${product.price}")
+                ],
+              ),
+            ),
+          );
+        }
+      ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +47,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_controller.appBarTitle.value),
       ),
-      body: Center(
-        child: Text(_controller.content.value),
-      ),
+      body: coffeeList(),
     );
   }
 }
